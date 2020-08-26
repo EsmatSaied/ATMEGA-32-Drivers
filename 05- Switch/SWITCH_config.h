9484ecc011e1928e09257fd3c33e756f8408339e
Esmat Saied <50220585+EsmatSaied@users.noreply.github.com>
2020-08-26 16:44:19 +0200

    Add files via upload

---
+++ 05- Switch/SWITCH_config.h
@@ -0,0 +1,19 @@
+/**************************************************/
+/**************************************************/
+/*************  Author: Esmat Saied   ************/
+/*************   Component: SWITCH     ************/
+/*************   Layer: HAL           ************/
+/*************   Version: 1.00        ************/
+/**************************************************/
+/**************************************************/
+
+#ifndef SWITCH_CONFIG_H_
+#define SWITCH_CONFIG_H_
+
+#ifndef SWITCH_CONFIG_H_
+#define SWITCH_CONFIG_H_
+
+#define SWITCH_PORT   			DIO_u8_PORTB
+#define SWITCH_PIN				DIO_u8_PIN0
+
+#endif
