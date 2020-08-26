05aab450287222fbef0bb2afbfb29480b48e9db9
Esmat Saied <50220585+EsmatSaied@users.noreply.github.com>
2020-08-26 16:15:33 +0200

    Add files via upload

---
+++ 01- character LCD/LCD_config.h
@@ -0,0 +1,11 @@
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+/*all of this i can change ports and pins any time according to tawselty 3adi*/
+#define LCD_u8_DATA_PORT  DIO_u8_PORTA
+#define LCD_u8_CTRL_PORT  DIO_u8_PORTB
+#define LCD_u8_RS_PIN  	  DIO_u8_PIN0
+#define LCD_u8_RW_PIN     DIO_u8_PIN1
+#define LCD_u8_E_PIN      DIO_u8_PIN2
+
+#endif
