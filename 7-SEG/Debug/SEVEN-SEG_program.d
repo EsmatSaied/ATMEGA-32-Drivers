cc08d1bd193c79ab427ff2628f09d5534d07ea43
Esmat Saied <50220585+EsmatSaied@users.noreply.github.com>
2020-08-26 16:12:06 +0200

    Add files via upload

---
+++ 7-SEG/Debug/SEVEN-SEG_program.d
@@ -0,0 +1,15 @@
+SEVEN-SEG_program.o SEVEN-SEG_program.o: ../SEVEN-SEG_program.c \
+  ../Std_Types.h ../DIO_interface.h ../PORT_interface.h \
+  ../SEVEN-SEG_config.h ../SEVEN-SEG_interface.h ../SEVEN-SEG_private.h
+
+../Std_Types.h:
+
+../DIO_interface.h:
+
+../PORT_interface.h:
+
+../SEVEN-SEG_config.h:
+
+../SEVEN-SEG_interface.h:
+
+../SEVEN-SEG_private.h:
